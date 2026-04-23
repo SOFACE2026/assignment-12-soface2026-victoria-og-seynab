8577813727e37f694d438a63d1030fe4678553e5
Vickiii <au792000@uni.au.dk>
2026-04-23 22:02:59 +0200

    ex1

--- include/revolver.hpp
+++ include/revolver.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "weapon.hpp"
+#include "projectile.hpp"
 class Revolver : public Weapon
 {
 public:
     Revolver();
 
 private:
-    virtual Projectile &get_projectile() override;
+    virtual Projectile &get_projectile() override; //gemmer projektilet som en del af objektet
 
     Projectile projectile;
 };
--- src/revolver.cpp
+++ src/revolver.cpp
@@ -1,10 +1,10 @@
 #include "revolver.hpp"
 
-Revolver::Revolver() : projectile(0)
+Revolver::Revolver() : projectile(0) //initialiserer projektilet i constructor
 {
 }
 
 Projectile &Revolver::get_projectile()
 {
-    return this->projectile;
+    return this->projectile; //returnerer en reference til projektilet, der er lagret i objektet
 }
